3bec7c265cace464a93c1e0e7da5b751377b9171
Lord_Arh <136067468+ArhNex@users.noreply.github.com>
2024-08-03 02:50:42 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,8 @@
 #! /bin/bash
 echo Ubuntu_tester from ArhNex
 
-is_ubuntu_tester=`docker images | grep ubuntu_tester | awk '{print $1}'`
-if [ $is_ubuntu_tester == ubuntu_tester ]
-    then
-        docker run -it --name tester --rm -v .:/project ubuntu_tester:arhnex /bin/zsh
-else
-    docker stop tester
-    docker rmi ubuntu_tester:arhnex
-    docker build -t ubuntu_tester:arhnex .
-    docker run -it --name tester --rm -v .:/project ubuntu_tester:arhnex /bin/zsh
-fi
 
-
+docker stop tester
+docker rmi ubuntu_tester:arhnex
+docker build -t ubuntu_tester:arhnex ./docker
+docker run -it --name tester --rm -v .:/project ubuntu_tester:arhnex /bin/zsh
